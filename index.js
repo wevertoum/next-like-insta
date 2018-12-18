f118c70716a7ed01980e033cd0ff2e74d3d0c412
Weverton Rodrigues <wevertonmixalt@gmail.com>
2018-12-18 09:28:00 -0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function yourFunction(){
+
+/* like */ javascript:var inputs = document.getElementsByClassName('glyphsSpriteHeart__outline__24__grey_9 u-__7'); for(var i=0; i<inputs.length;i++) { inputs[i].click(); }
+
+/* next */ javascript:var inputs = document.getElementsByClassName('HBoOv coreSpriteRightPaginationArrow'); for(var i=0; i<inputs.length;i++) { inputs[i].click(); }
+setTimeout(yourFunction, 5000);
+}
+yourFunction();
+
